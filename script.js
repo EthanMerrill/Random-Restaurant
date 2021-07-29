07b265461b83ef889e8f360e96eb6ed28d73ed79
Ethan <eth22mer@gmail.com>
2021-07-29 17:13:01 -0400

    Added walking and driving times.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ async function geoFindMe() {
         //     const longitude = position.coords.longitude;
 
         //     statusElement.textContent = `Success, Latitude: ${latitude}, Longitude: ${longitude}`
-            
+
         // }
 
         // function error() {
@@ -17,7 +17,7 @@ async function geoFindMe() {
         if (!navigator.geolocation) {
             console.log('Geolocation is not supported by your browser');
         } else {
-            
+
             navigator.geolocation.getCurrentPosition(position, error)
 
         }
@@ -25,141 +25,157 @@ async function geoFindMe() {
 
 
 }
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-// function initMap(lat, long) {
-//     // Create the map.
-
-//     const coordinates = { lat: lat, lng: long};
-//     const map = new google.maps.Map(document.getElementById("map"), {
-//         center: coordinates,
-//         zoom: 17,
-//         mapId: "8d193001f940fde3",
-//     });
-//     // Create the places service.
-//     const service = new google.maps.places.PlacesService(map);
-//     let getNextPage;
-//     const moreButton = document.getElementById("more");
-
-//     moreButton.onclick = function () {
-//         moreButton.disabled = true;
-
-//         if (getNextPage) {
-//             getNextPage();
-//         }
-//     };
-//     // Perform a nearby search.
-//     service.nearbySearch(
-//         { location: coordinates, radius: 800, type: "restaurant" },
-//         (results, status, pagination) => {
-//             if (status !== "OK" || !results) return;
-//             addPlaces(results, map);
-//             moreButton.disabled = !pagination || !pagination.hasNextPage;
-
-//             if (pagination && pagination.hasNextPage) {
-//                 getNextPage = () => {
-//                     // Note: nextPage will call the same handler function as the initial call
-//                     pagination.nextPage();
-//                 };
-//             }
-//         }
-//     );
-// }
-
-// function addPlaces(places, map) {
-//     const placesList = document.getElementById("places");
-
-//     for (const place of places) {
-//         if (place.geometry && place.geometry.location) {
-//             const image = {
-//                 url: place.icon,
-//                 size: new google.maps.Size(71, 71),
-//                 origin: new google.maps.Point(0, 0),
-//                 anchor: new google.maps.Point(17, 34),
-//                 scaledSize: new google.maps.Size(25, 25),
-//             };
-//             new google.maps.Marker({
-//                 map,
-//                 icon: image,
-//                 title: place.name,
-//                 position: place.geometry.location,
-//             });
-//             const li = document.createElement("li");
-//             li.textContent = place.name;
-//             placesList.appendChild(li);
-//             li.addEventListener("click", () => {
-//                 map.setCenter(place.geometry.location);
-//             });
-//         }
-//     }
-// }
-// document.querySelector('#find-me').addEventListener('click', geoFindMe);
 
 // Simple location search function
-function getOnePlace(lat, long){
+function getOnePlace(lat, long) {
 
     const coordinates = { lat: lat, lng: long };
-    console.log(coordinates)
     var request = {
         query: 'restaurant',
         openNow: "True",
         rankBy: "google.maps.places.RankBy.DISTANCE",
-        fields: ['name', 'geometry', 'opening_hours', 'price_level', 'place_id', 'open_now' ]
+        fields: ['name', 'geometry', 'opening_hours', 'price_level', 'place_id', 'open_now']
     };
 
-    map = new google.maps.Map(
+    const map = new google.maps.Map(
         document.getElementById('map'), { center: coordinates, zoom: 25 });
+
 
     var service = new google.maps.places.PlacesService(map);
 
-    service.nearbySearch({ location: coordinates, radius: 25000, type: "restaurant" }, (results, status, opening_hours, pagination) =>{
+    service.nearbySearch({ location: coordinates, radius: 25000, type: "restaurant" }, (results, status, opening_hours, pagination) => {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
-            // for (let i = 0; i < results.length; i++) {
-            //     console.log(results[i].opening_hours.open_now)
-            //     console.log(i)
-            // }
-                var i = getRandomArbitrary(0, results.length-1)
-                console.log(i, results.length)
-                place = results[i]
-                console.log(i, place, results)
-                // Get place Details
-                var detailsRequest = {
-                    placeId: place.place_id,
-                    fields: ['name', 'rating', 'website', 'reviews']
-                }
-                service = new google.maps.places.PlacesService(map);
-                service.getDetails(detailsRequest, callback);
+            // chose a random place
+            var i = getRandomArbitrary(0, results.length - 1)
+            place = results[i]
+            console.log(place.vicinity)
+            // Get place Details
+            var detailsRequest = {
+                placeId: place.place_id,
+                fields: ['name', 'rating', 'website', 'reviews']
+            }
+
+            service = new google.maps.places.PlacesService(map);
+            service.getDetails(detailsRequest, callback);
             // 
-                function callback(detailedPlace, status) {
-                    if (status == google.maps.places.PlacesServiceStatus.OK) {
-                        console.log(detailedPlace)
-                        document.getElementById('restaurant-idea').innerHTML = `Go to <a href='${detailedPlace.website}'><i>${detailedPlace.name}</i></a>`
-                        var navLink = document.createElement("a")
-                        navLink.href = `https://www.google.com/maps/dir/?api=1&origin=${coordinates.lat}+${coordinates.lng}&destination=${detailedPlace.name}&travelmode=walking`
-                        navLink.innerHTML = " "
-                        wrap(document.getElementById('lets-go-button'), navLink)
+            function callback(detailedPlace, status) {
+                if (status == google.maps.places.PlacesServiceStatus.OK) {
 
-                        
-                    }
+                    console.log(detailedPlace)
+                    document.getElementById('restaurant-idea').innerHTML = `Go to <a href='${detailedPlace.website}'><i>${detailedPlace.name}</i></a>`
+                    var navLink = document.createElement("a")
+                    navLink.href = `https://www.google.com/maps/dir/?api=1&origin=${coordinates.lat}+${coordinates.lng}&destination=${detailedPlace.name}&travelmode=walking`
+                    navLink.innerHTML = " "
+                    wrap(document.getElementById('lets-go-button'), navLink)
+
+
                 }
-
-
-            // }
-
+            }
         }
     });
 }
+
+var getPlaceDetails = (placeid) => {
+    service = new google.maps.places.PlacesService(map); // can refactor to only initialize this once...
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const placeDetails = service.getDetails(
+                {
+                    placeId: placeid,
+                    fields: ['name', 'rating', 'website', 'reviews']
+                },(placeDetails, status) => {
+                    resolve(placeDetails)
+                })
+           
+        }, 2000)
+
+    })
+
+}
+
+var searchNearby = (location, radius) => {
+    const map = new google.maps.Map(document.getElementById('map'), { center: location, zoom: 25 });
+    var placesService = new google.maps.places.PlacesService(map);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const placesList = placesService.nearbySearch(
+                {
+                    location:location, 
+                    radius:radius, 
+                    type:"restaurant"
+                }, (results, status) => {
+                    if (status === google.maps.places.PlacesServiceStatus.OK) {
+                        resolve(results)
+                    }
+                });
+        }, 2000)
+    })
+}
+const coordinates = { lat: 38.8726784, lng: -77.0899968 }
+
+//helper functions below:
+
+
+var getTravelTimes = (originPoint, destinationList, walking) => {
+    var matrix = new google.maps.DistanceMatrixService();
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (walking==true){
+                const distanceData = matrix.getDistanceMatrix(
+                    {
+                        origins: [originPoint],
+                        destinations: destinationList,
+                        travelMode: 'WALKING',
+                        unitSystem: google.maps.UnitSystem.IMPERIAL
+                    });
+                resolve(distanceData)
+            } else if (walking == false ){
+                const distanceData = matrix.getDistanceMatrix(
+                    {
+                        origins: [originPoint],
+                        destinations: destinationList,
+                        travelMode: 'DRIVING',
+                        unitSystem: google.maps.UnitSystem.IMPERIAL
+                    });
+                resolve(distanceData)
+            }
+            reject('took too long')
+            
+        }, 2000)
+    })
+}
+
 
 function wrap(el, wrapper) {
     el.parentNode.insertBefore(wrapper, el);
     wrapper.appendChild(el);
 }
 
+function getRandomArbitrary(min, max) {
+    return Math.round((Math.random() * (max - min) + min), 0);
+}
+
 async function main() {
     var position = await geoFindMe();
-    getOnePlace(position.coords.latitude, position.coords.longitude)
-    document.getElementById('info-circle').addEventListener('click', function(event) {
+    var placesList = await searchNearby(coordinates, 25000)
+    var randomNumber = await getRandomArbitrary(0, placesList.length-1)
+
+    const travelTimeWalking = await getTravelTimes(originPoint = new google.maps.LatLng(position.coords.latitude, position.coords.longitude), [placesList[randomNumber].vicinity], true)
+    var tripinfo 
+    // if walking takes less than 20 mins, do that. if longer, get driving directions and use that as the trip info
+    if (travelTimeWalking.rows[0].elements[0].duration.value<2400){
+        tripinfo = travelTimeWalking
+    } else {
+        tripinfo = await getTravelTimes(originPoint = new google.maps.LatLng(position.coords.latitude, position.coords.longitude), [placesList[randomNumber].vicinity], false)
+    }
+    var placeSelection = placesList[randomNumber]
+    var placeDetails = await getPlaceDetails(placeSelection.place_id)
+    console.log(tripinfo.rows[0].elements[0].duration)
+    document.getElementById('restaurant-idea').innerHTML = `Drive ${tripinfo.rows[0].elements[0].duration.text} to <a href='${placeDetails.website}'><i>${placeSelection.name}</i></a>`
+    console.log(tripinfo.rows[0].elements[0].distance.text)
+
+    // getOnePlace(position.coords.latitude, position.coords.longitude)
+    document.getElementById('info-circle').addEventListener('click', function (event) {
         document.getElementById('info-box').classList.toggle('invisible')
         document.getElementById('info-circle').classList.toggle('info-selected')
         // document.getElementById('info-box').setAttribute('visibility', 'visible')
@@ -172,7 +188,3 @@ async function main() {
     })
 }
 main()
-
-function getRandomArbitrary(min, max) {
-    return Math.round((Math.random() * (max - min) + min),0);
-}
